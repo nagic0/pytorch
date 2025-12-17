b005baa7d5bf958a245a3702c3fab0cb89b96bc1
Nagico <nagico@qq.com>
2025-12-17 19:21:28 +0800

    fix: data ptr

--- aten/src/ATen/native/mps/operations/UsmShare.mm
+++ aten/src/ATen/native/mps/operations/UsmShare.mm
@@ -6,7 +6,7 @@ namespace at::native {
 
 // Implementation for MPS backend
 Tensor usm_share_from_mps(const Tensor& self, const c10::Storage& src) {
-  void* ptr = src.data_ptr();
+  void* ptr = src.data_ptr().get();
   size_t size = src.nbytes();
 
   TORCH_CHECK(
